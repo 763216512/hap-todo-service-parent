e31390f720be9028735bc8938699d448a99446b1
21950 <gaokun.huang@hand-china.com>
2019-01-11 16:41:44 +0800

    Initial commit

---
+++ hap-todo-service-parent/hap-todo-service/src/main/java/com/hand/todo/demo/TodoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hand.todo.demo;
+
+/**
+ * @author gaokun.huang@hand-china.com
+ * @version 1.0
+ * @name
+ * @description
+ * @date 2019/1/11
+ */
+
+public class TodoServiceApplication {
+}
